ea47bb66a40738407975f8e3869c4516f854c6eb
Gleb <glebsmaklasins@gmail.com>
2020-08-05 20:13:14 +0100

    Third practice ,
    func

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,76 @@
-let numberOfFilms = +prompt ("skoljko filjmov vi posmotreli","");
-function start(){
-    numberOfFilms = +prompt ("skoljko filjmov vi posmotreli", "");
-    
-    while(numberOfFilms =="" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    
-        numberOfFilms = +prompt ("skoljko filjmov vi posmotreli", "");
-        
-    }
-}
-start();
+
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies:{},
     actors:{},
     genres:[],
-    privat: false
-};
-
-function rememberMyFilms(){
-    for(let i=0;i<2;i++){
-        const a = prompt("odin iz poslednih prosmotrevajemih filjmov" ,""),
-            b= prompt("naskoljko ocenivaesh ?", "");
-    
-        if(a!=null && b !=null && a!= "" && b != "" && a.length <50){
-            personalMovieDB.movies[a] = b;
-    
-            console.log("done");
+    privat: false,
+    start : function (){
+        personalMovieDB.count = +prompt ("skoljko filjmov vi posmotreli", "");
+        
+        while(personalMovieDB.count =="" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+        
+            personalMovieDB.count = +prompt ("skoljko filjmov vi posmotreli", "");
+            
+        }
+    },
+    rememberMyFilms:function(){
+        for(let i=0;i<2;i++){
+            const a = prompt("odin iz poslednih prosmotrevajemih filjmov" ,""),
+                b= prompt("naskoljko ocenivaesh ?", "");
+        
+            if(a!=null && b !=null && a!= "" && b != "" && a.length <50){
+                personalMovieDB.movies[a] = b;
+        
+                console.log("done");
+            }else{
+                console.log("error");
+                i--;
+            }
+        
+        }
+    },
+    detectPersonalLevel:function(){
+        if(personalMovieDB.count < 10){
+            console.log("malo filjmov smotrish");
+        }else if (personalMovieDB.count >10 && personalMovieDB.count <30){
+            console.log("vi norm smotrite kinco");
+        }else if (personalMovieDB.count >= 30){
+            console.log("kinomannnn");
         }else{
             console.log("error");
-            i--;
         }
-    
+    },
+    showMyDB:function(hidden){
+        if(!hidden){
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB:function(){
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        }else{
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres:function(){
+        for(let i = 1; i<=3;i++){
+            let genre=prompt(`vash ljubimij zhanr pod monerom ${i}`);
+            if(genre==="" ||genre == null){
+                console.log("vi vveli nekonkretnie dannie");
+            }else{
+                personalMovieDB.genres[i-1] =genre;
+            }
+         
+        }
+        personalMovieDB.genres.forEach((item,i)=>{
+            console.log(`ljubimj zhanr ${i+1} - eto ${item}`);
+        });
     }
-}
-rememberMyFilms();
+};
 
-function detectPersonalLevel(){
-    if(personalMovieDB.count < 10){
-        console.log("malo filjmov smotrish");
-    }else if (personalMovieDB.count >10 && personalMovieDB.count <30){
-        console.log("vi norm smotrite kinco");
-    }else if (personalMovieDB.count >= 30){
-        console.log("kinomannnn");
-    }else{
-        console.log("error");
-    }
-}
-detectPersonalLevel();
 
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
 
-function writeYourGenres(){
-    for(let i = 1; i<=3;i++){
-        const genre=prompt(`vash ljubimij zhanr pod monerom ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-    }
-}
-writeYourGenres();
 console.log(personalMovieDB);
 
