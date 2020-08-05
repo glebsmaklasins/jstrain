09b7dfc858275d0c017322fa8fe3919c58e1e0b6
Gleb <glebsmaklasins@gmail.com>
2020-08-05 13:57:25 +0100

    For  cycle ,
    Cycle and condition practice

--- src/js/script.js
+++ src/js/script.js
@@ -8,11 +8,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("odin iz poslednih prosmotrevajemih filjmov" ,""),
-        b= prompt("naskoljko ocenivaesh ?", ""),
-        c = prompt("odin iz poslednih prosmotrevajemih filjmov" ,""),  
-        d= prompt("naskoljko ocenivaesh ?", "");
+for(let i=0;i<2;i++){
+    const a = prompt("odin iz poslednih prosmotrevajemih filjmov" ,""),
+        b= prompt("naskoljko ocenivaesh ?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if(a!=null && b !=null && a!= "" && b != "" && a.length <50){
+        personalMovieDB.movies[a] = b;
+
+        console.log("done");
+    }else{
+        console.log("error");
+        i--;
+    }
+
+}
+if(personalMovieDB.count < 10){
+    console.log("malo filjmov smotrish");
+}else if (personalMovieDB.count >10 && personalMovieDB.count <30){
+    console.log("vi norm smotrite kinco");
+}else if (personalMovieDB.count >= 30){
+    console.log("kinomannnn");
+}else{
+    console.log("error");
+}
+
 console.log(personalMovieDB);
