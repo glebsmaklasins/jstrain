0e25eff8d4aaa8286c685986ef69b13fa8920ee9
Gleb <example@gamil.com>
2020-08-05 11:47:11 +0100

    firstcommit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isChecked = true;
+        isClose= true ;
+
+console.log(isChecked && isClose)
